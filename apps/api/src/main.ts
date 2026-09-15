23096e6c63c4e769e0199b4d796362d94d4361e6
Paula Beatriz <paula.gomes@estudante.ifto.edu.br>
2026-09-14 21:38:10 -0300

    fix: corrige inicializacao do cookie parser

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
